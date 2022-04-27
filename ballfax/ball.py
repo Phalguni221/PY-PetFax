b29535dc8359bc92c8e8538872ab55290faaf63f
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-04-27 18:08:45 -0400

    Add files via upload

--- ballfax/ball.py
+++ ballfax/ball.py
@@ -1,31 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask import (Blueprint, render_template, redirect, request)
 
-def create_app():
-    app = Flask(__name__)
 
-# factory 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Phalguni2025@localhost:5432/ballfax'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+{
+    'common_name': 'Ball python',
+    'scientific_name': 'Python regius',
+    'conservation_status': 'Near threatened',
+    'native_habitat': 'Forest, savanna, shrubland, grassland',
+    'fun_fact': 'Ball pythons received their common name
+        from their behavior of curling up into a ball when threatened.'
+}
 
-#register model blueprint
-    from . import ballmodels 
-    ballmodels.db.Ball(app) 
-    migrate = Migrate(app, ballmodels.db)
 
-bp = Blueprint('ball', __name__, url_prefix="/ball")
-    
-    # index route
-    @app.route('/ballfax')
-    def ballindex(): 
-            return 'Hello, BallFax!'
-    
-
-    # # register pet blueprint 
-    # from . import pet
-    # app.register_blueprint(pet.bp)
-
-    # #register fact blueprint
-    # from . import fact
-    # app.register_blueprint(fact.bp)
--- ballfax/ballmodels.py
+++ ballfax/ballmodels.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Fact(db.ballfaxModel):
-    __tablename__ = 'facts'
+class ballpy(db.Model):
+    __tablename__ = 'reptiles'
     id = db.Column(db.Integer, primary_key = True)
     submitter = db.Column(db.String(250))
     fact = db.Column(db.Text)
