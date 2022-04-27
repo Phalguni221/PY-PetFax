d03eb64067601703cd893c3cedee97d662b62277
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-04-27 14:33:19 -0400

    Added Ballfax

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,11 +1,20 @@
 # config                    
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
 
+# factory 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Phalguni2025@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
 
+
+#register model blueprint
+    from . import models 
+    models.db.init_app(app) 
+    migrate = Migrate(app, models.db)
     
     # index route
     @app.route('/')
@@ -16,9 +25,10 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    #register fact blueprint
     from . import fact
     app.register_blueprint(fact.bp)
+ 
 
     # return the app 
     return app
-
